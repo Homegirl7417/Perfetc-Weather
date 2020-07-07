4a5b3e0f5e18a696382e63cc7e06604cd5dd45ed
HyunKyeong <hk7417@emoticbox.com>
2020-07-07 15:26:59 +0900

    After install expo-linear-gradient

--- weather.js
+++ weather.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { PropTypes } from "prop-types";
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { LinearGradient } from 'expo-linear-gradient';
 export default function Weather ({temp}) {
     return (
         <View style={styles.container}>
